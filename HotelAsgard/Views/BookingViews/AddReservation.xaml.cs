e7d9056b0dfac3c7a8311263d2e65a66b107183e
FranMor97 <fran.moreno.1234@gmail.com>
2025-01-14 12:10:39 +0100

    Added 1 view

--- HotelAsgard/Views/BookingViews/AddReservation.xaml.cs
+++ HotelAsgard/Views/BookingViews/AddReservation.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,24 @@ namespace HotelAsgard.Views.BookingViews
         public AddReservation()
         {
             InitializeComponent();
+
+            Habitaciones = new ObservableCollection<Habitacion>
+            {
+                new Habitacion { Imagen = "Images/hab_odin.jpg", Nombre = "Habitación Odin", Precio = "36€ / noche" },
+                new Habitacion { Imagen = "Images/hab_valhalla.jpg", Nombre = "Habitación Valhalla", Precio = "42€ / noche" },
+                new Habitacion { Imagen = "Images/hab_freyja.jpg", Nombre = "Habitación Freyja", Precio = "50€ / noche" }
+            };
+
+            DataContext = this;
+        }
+
+        public ObservableCollection<Habitacion> Habitaciones { get; }
+
+        public class Habitacion
+        {
+            public string Imagen { get; set; }
+            public string Nombre { get; set; }
+            public string Precio { get; set; }
         }
     }
 }
--- HotelAsgard/Views/BookingViews/BookByRoom.xaml.cs
+++ HotelAsgard/Views/BookingViews/BookByRoom.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,10 @@ namespace HotelAsgard.Views.BookingViews
         public BookByRoom()
         {
             InitializeComponent();
+
         }
     }
+    
+
+
 }
